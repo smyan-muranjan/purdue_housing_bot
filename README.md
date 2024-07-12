323439430f379c0a8f74eb3d92cb811b66b9d5ad
smuranjan1 <smyan.muranjan@gmail.com>
2024-07-11 23:48:06 -0500

    Finished code.

--- README.md
+++ README.md
@@ -5,9 +5,7 @@ Prerequisites:
 Install Tesseract OCR from: https://github.com/tesseract-ocr/tesseract
 
 Install following python packages using pip install:
-
     - smtplib
     - opencv-python
     - numpy
     - pytesseract
-Use a tool such as Auto Hot Key to find the coordinates on screen to check for updates from.
--- src/purdue_housing_bot.py
+++ src/purdue_housing_bot.py
@@ -1,59 +1,80 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
+from tkinter import font as tkFont
 import re
-import smtplib, ssl, cv2, time, pytesseract
+import smtplib, ssl, cv2, pytesseract
 import numpy as np
-from PIL import ImageGrab
+from PIL import ImageGrab, ImageTk, Image  
 from email.message import EmailMessage
 
 class PurdueHousingBot:
     email_re = r"""(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 
+    window: tk.Tk
+    sender_email: tk.StringVar
+    sender_password: tk.StringVar
+    receiver_emails: tk.StringVar
+    timer: tk.IntVar
+    isRunning: tk.BooleanVar
+    message: tk.StringVar
+
     def __init__(self, root):
         self.window = root
         self.window.title("Purdue Housing Bot")
-        
+        self.window.geometry("475x250")
+        self.window.minsize(475, 250)
+        default_font = tkFont.nametofont("TkDefaultFont")
+        default_font.configure(size=10) 
+        self.window.option_add("*Font", default_font)
+        self.window.iconbitmap("purdue_housing_bot.ico")
         self.sender_email = tk.StringVar(self.window)
         self.sender_password = tk.StringVar(self.window)
         self.receiver_emails = tk.StringVar(self.window)
         self.timer = tk.IntVar(self.window, value=20)
         self.isRunning = tk.BooleanVar(self.window, value=False)
-        self.message = tk.StringVar(self.window)
+        self.message = tk.StringVar(self.window, value="Not Running.")
         
         self.create_widgets()
         self.window.after(0, self.run_program)
     
     def create_widgets(self):
-        email_label = ttk.Label(self.window, text="Enter your email:")
-        email_label.pack()
-        self.email_entry = ttk.Entry(self.window, textvariable=self.sender_email)
-        self.email_entry.pack()
+        email_label = ttk.Label(self.window, text="Enter sender email:")
+        email_label.grid(row=0, column=0)
+        self.email_entry = ttk.Entry(self.window, textvariable=self.sender_email, width=32)
+        self.email_entry.grid(row=1, column=0, pady=(0, 10))
         
-        password_label = ttk.Label(self.window, text="Enter your password:")
-        password_label.pack()
-        self.password_entry = ttk.Entry(self.window, textvariable=self.sender_password, show='*')
-        self.password_entry.pack()
+        password_label = ttk.Label(self.window, text="Enter sender password:")
+        password_label.grid(row=2, column=0)
+        self.password_entry = ttk.Entry(self.window, textvariable=self.sender_password, show='*', width=32)
+        self.password_entry.grid(row=3, column=0, pady=(0, 10))
+        image = Image.open("purdue_logo.png")
+        image = image.resize((150, 80))
+        test = ImageTk.PhotoImage(image)
+        image_label = tk.Label(self.window, image=test)
+        image_label.image = test    
+        image_label.grid(row=0, column=1, rowspan=4)
         
-        receiver_email_label = ttk.Label(self.window, text="Enter the emails of the people you want to send to:")
-        receiver_email_label.pack()
-        self.receiver_email_entry = ttk.Entry(self.window, textvariable=self.receiver_emails)
-        self.receiver_email_entry.pack()
+        receiver_email_label = ttk.Label(self.window, text="Enter the emails of the people you want to send to, seperated by commas:")
+        receiver_email_label.grid(row=4, column=0, columnspan=2, padx=(10, 0))
+        self.receiver_email_entry = ttk.Entry(self.window, textvariable=self.receiver_emails, width=64)
+        self.receiver_email_entry.grid(row=5, column=0, columnspan=2, padx=(10, 0), pady=(0, 10))
         
-        time_label = ttk.Label(self.window, textvariable=self.timer)
-        time_label.pack()
+
+        time_label = ttk.Label(self.window, textvariable=self.timer, font="TkDefaultFont 30")
+        time_label.grid(row=6, column=1, rowspan=2)
         
+
         self.test_btn = ttk.Button(self.window, text="Test", command=self.send_test_email)
-        self.test_btn.pack()
+        self.test_btn.grid(row=6, column=0)
 
         self.start_btn = ttk.Button(self.window, text="Start", command=self.start_program)
-        self.start_btn.pack()
+        self.start_btn.grid(row=7, column=0)
         
         self.stop_btn = ttk.Button(self.window, text="Stop", state="disabled", command=self.stop_program)
-        self.stop_btn.pack()
+        self.stop_btn.grid(row=8, column=0)
 
         self.message_label = ttk.Label(self.window, textvariable=self.message)
-        self.message_label.pack()
+        self.message_label.grid(row=8, column=1)
     
     def validate_email(self) -> bool:
         return bool(re.match(self.email_re, self.sender_email.get()))
@@ -90,6 +111,7 @@ class PurdueHousingBot:
             self.isRunning.set(True)
             self.stop_btn.config(state="normal")
             self.start_btn.config(state="disabled")
+            self.message.set("Running.")
         
     def stop_program(self):
         self.email_entry.config(state='normal')
@@ -98,6 +120,7 @@ class PurdueHousingBot:
         self.isRunning.set(False)
         self.stop_btn.config(state="disabled")
         self.start_btn.config(state="normal")
+        self.message.set("Not Running.")
     
     def run_program(self):
         if self.isRunning.get():
@@ -124,6 +147,7 @@ class PurdueHousingBot:
     def send_test_email(self):
         self.send_email(subject="Purdue Housing Bot: Test Email", body="This is a test email. Please check that all emails recieved the message.")
         self.message.set("Test email sent.")
+        self.window.after(5000, lambda: self.message.set("Not Running."))
 
 
     def send_email(self, subject: str, body: str):
