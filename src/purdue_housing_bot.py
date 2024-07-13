f8235b94eb3b6b48846bbe3e8376ecc96277efb2
smuranjan1 <smyan.muranjan@gmail.com>
2024-07-13 18:57:47 -0500

    Formatted using pep8 guidelines.

--- src/purdue_housing_bot.py
+++ src/purdue_housing_bot.py
@@ -1,13 +1,20 @@
+"""This module contains the PurdueHousingBot class which is a GUI application"""
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 from tkinter import font as tkFont
-import smtplib, ssl, cv2, pytesseract, re
-import numpy as np
-from PIL import ImageGrab, ImageTk, Image  
+import smtplib
+import ssl
+import re
 from email.message import EmailMessage
 import os.path
+import cv2
+import pytesseract
+import numpy as np
+from PIL import ImageGrab, ImageTk, Image
+
 
 class PurdueHousingBot:
+    """This class is the Purdue Housing Bot GUI application"""
     email_re: str = (
         r"(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)"
         r'*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]'
@@ -23,219 +30,229 @@ class PurdueHousingBot:
     receiver_emails: tk.StringVar
     tesseract_location: tk.StringVar
     timer: tk.IntVar
-    isRunning: tk.BooleanVar
+    is_running: tk.BooleanVar
     message: tk.StringVar
 
-    def __init__(self, root):
-        self.window = root
+    def __init__(self, master: tk.Tk):
+        self.window = master
         self.window.title("Purdue Housing Bot")
         self.window.geometry("475x325")
         self.window.minsize(475, 325)
         default_font = tkFont.nametofont("TkDefaultFont")
-        default_font.configure(size=10) 
+        default_font.configure(size=10)
         self.window.option_add("*Font", default_font)
-        self.window.iconbitmap("src\purdue_housing_bot.ico")
+        self.window.iconbitmap("src\\purdue_housing_bot.ico")
         self.sender_email = tk.StringVar(self.window)
         self.sender_password = tk.StringVar(self.window)
         self.receiver_emails = tk.StringVar(self.window)
         self.tesseract_location = tk.StringVar(self.window)
         self.timer = tk.IntVar(self.window, value=20)
-        self.isRunning = tk.BooleanVar(self.window, value=False)
+        self.is_running = tk.BooleanVar(self.window, value=False)
         self.message = tk.StringVar(self.window, value="Not Running.")
-        
+
         self.create_widgets()
         self.window.after(0, self.run_program)
-    
+
     def create_widgets(self):
+        """Function creating the widgets for the GUI"""
         email_label = ttk.Label(self.window, text="Enter sender email:")
         email_label.grid(row=0, column=0)
         self.email_entry = ttk.Entry(
-            self.window, 
-            textvariable=self.sender_email, 
+            self.window,
+            textvariable=self.sender_email,
             width=32
-            )
+        )
         self.email_entry.grid(
-            row=1, 
-            column=0, 
+            row=1,
+            column=0,
             pady=(0, 10)
-            )
-        
+        )
+
         password_label = ttk.Label(self.window, text="Enter sender password:")
         password_label.grid(row=2, column=0)
         self.password_entry = ttk.Entry(
-            self.window, 
-            textvariable=self.sender_password, 
-            show='*', 
+            self.window,
+            textvariable=self.sender_password,
+            show='*',
             width=32
-            )
+        )
         self.password_entry.grid(
-            row=3, 
-            column=0, 
+            row=3,
+            column=0,
             pady=(0, 10)
-            )
+        )
         image = Image.open("src/purdue_logo.png")
         image = image.resize((150, 80))
         test = ImageTk.PhotoImage(image)
         image_label = tk.Label(self.window, image=test)
-        image_label.image = test    
+        image_label.image = test
         image_label.grid(
-            row=0, 
-            column=1, 
+            row=0,
+            column=1,
             rowspan=4
-            )
-        
+        )
+
         receiver_email_label = ttk.Label(
-            self.window, 
+            self.window,
             text=("Enter the emails of the people you want to send to"
-                ", seperated by commas:")
-            )
+                  ", seperated by commas:")
+        )
         receiver_email_label.grid(
-            row=4, 
-            column=0, 
-            columnspan=2, 
+            row=4,
+            column=0,
+            columnspan=2,
             padx=(10, 0)
-            )
+        )
         self.receiver_email_entry = ttk.Entry(
             self.window, textvariable=self.receiver_emails, width=64)
         self.receiver_email_entry.grid(
-            row=5, 
-            column=0, 
-            columnspan=2, 
-            padx=(10, 0), 
+            row=5,
+            column=0,
+            columnspan=2,
+            padx=(10, 0),
             pady=(0, 10)
-            )
-        
+        )
+
         tesseract_label = ttk.Label(
             self.window,
             text="Enter the location of the tesseract executable:"
         )
         tesseract_label.grid(
-            row=6, 
+            row=6,
             column=0, columnspan=2
-            )
+        )
         self.tesseract_entry = ttk.Entry(
-            self.window, 
-            width=48, 
+            self.window,
+            width=48,
             textvariable=self.tesseract_location
-            )
+        )
         self.tesseract_entry.grid(
-            row=7, 
-            column=0, 
+            row=7,
+            column=0,
             pady=(0, 10),
             padx=(30, 0),
             columnspan=2,
             sticky="w"
-            )
+        )
         self.browse_file_btn = ttk.Button(
-            self.window, 
-            text="Browse", 
+            self.window,
+            text="Browse",
             width=8,
             command=lambda: self.tesseract_location.set(
                 filedialog.askopenfilename()
-                )
+            )
         )
         self.browse_file_btn.grid(
-            row=7, 
-            column=1, 
-            pady=(0, 10), 
+            row=7,
+            column=1,
+            pady=(0, 10),
             padx=(70, 0)
-            )
-        
+        )
 
         self.time_label = ttk.Label(
-            self.window, 
-            textvariable=self.timer, 
+            self.window,
+            textvariable=self.timer,
             font="TkDefaultFont 30"
-            )
+        )
         self.time_label.grid(row=8, column=1, rowspan=2)
-        
 
         self.test_btn = ttk.Button(
-            self.window, 
-            text="Test", 
+            self.window,
+            text="Test",
             command=self.send_test_email
-            )
+        )
         self.test_btn.grid(row=8, column=0)
 
         self.start_btn = ttk.Button(
-            self.window, 
-            text="Start", 
+            self.window,
+            text="Start",
             command=self.start_program
-            )
+        )
         self.start_btn.grid(row=9, column=0)
-        
+
         self.stop_btn = ttk.Button(
-            self.window, 
-            text="Stop", 
-            state="disabled", 
+            self.window,
+            text="Stop",
+            state="disabled",
             command=self.stop_program
-            )
+        )
         self.stop_btn.grid(row=10, column=0)
 
         self.message_label = ttk.Label(self.window, textvariable=self.message)
         self.message_label.grid(row=10, column=1)
-    
+
     def validate_email(self) -> bool:
+        """Function to validate the sender email"""
         return bool(re.match(self.email_re, self.sender_email.get()))
-    
+
     def validate_password(self) -> bool:
+        """Function to validate the sender password"""
         return len(self.sender_password.get()) > 0
-    
+
     def process_reciever_emails(self) -> list[str]:
+        """Function convert the receiver emails string to a list"""
         return list(map(
             lambda email: email.strip(), self.receiver_emails.get().split(",")
-            ))
+        ))
 
     def validate_emails(self):
+        """Function to validate the receiver emails"""
         emails = self.process_reciever_emails()
         return all(map(lambda email: re.match(self.email_re, email), emails))
-    
+
     def validate_tesseract_location(self):
+        """Function to validate the tesseract location"""
         return os.path.isfile(self.tesseract_location.get())
 
     def validate_inputs(self) -> bool:
+        """Function to validate all inputs"""
         valid_sender_email = self.validate_email()
         valid_password = self.validate_password()
         valid_reciever_emails = self.validate_emails()
         valid_tesseract_location = self.validate_tesseract_location()
-        
-        if valid_sender_email and valid_password and valid_reciever_emails and valid_tesseract_location:
-            return True
-        else:
-            error_message = (
-                ("" if valid_sender_email else "Sender email is invalid.\n") + \
-                ("" if valid_password else "Password is invalid.\n") + \
-                ("" if valid_reciever_emails else "Receiver emails are invalid.\n") + \
-                ("" if valid_tesseract_location else "Tesseract location is invalid.\n")
-                )
-            messagebox.showerror(title="Error", message=error_message)
+
+        if (not valid_sender_email
+            or not valid_password
+            or not valid_reciever_emails
+            or not valid_tesseract_location):
             return False
+        error_message = (
+            ("" if valid_sender_email else "Sender email is invalid.\n") +
+            ("" if valid_password else "Password is invalid.\n") +
+            ("" if valid_reciever_emails else "Receiver emails are invalid.\n") +
+            ("" if valid_tesseract_location else "Tesseract location is invalid.\n")
+        )
+        messagebox.showerror(title="Error", message=error_message)
+        return True
 
     def start_program(self):
+        """Function to start the program"""
         if self.validate_inputs():
             self.email_entry.config(state='disabled')
             self.password_entry.config(state='disabled')
             self.receiver_email_entry.config(state='disabled')
             self.tesseract_entry.config(state='disabled')
-            self.isRunning.set(True)
+            self.is_running.set(True)
             self.stop_btn.config(state="normal")
             self.start_btn.config(state="disabled")
             self.browse_file_btn.config(state="disabled")
             self.message.set("Running.")
-        
+
     def stop_program(self):
+        """Function to stop the program"""
         self.email_entry.config(state='normal')
         self.password_entry.config(state='normal')
         self.receiver_email_entry.config(state='normal')
         self.tesseract_entry.config(state='normal')
-        self.isRunning.set(False)
+        self.is_running.set(False)
         self.stop_btn.config(state="disabled")
         self.start_btn.config(state="normal")
         self.browse_file_btn.config(state="normal")
         self.message.set("Not Running.")
-    
+
     def run_program(self):
-        if self.isRunning.get():
+        """Function to run the program"""
+        if self.is_running.get():
             if self.timer.get() == 0:
                 self.check_screen()
                 self.timer.set(20)
@@ -244,58 +261,63 @@ class PurdueHousingBot:
         self.window.after(1000, self.run_program)
 
     def check_screen(self):
-        expectedOutput = "We couldn't find any available rooms."
+        """Function to check the screen for available housing"""
+        expected_output = "We couldn't find any available rooms."
         result = self.screen_to_string()
         print(result)
-        if result is not None and expectedOutput not in result:
+        if result is not None and expected_output not in result:
             self.send_email(
-                subject="Purdue Housing Bot: New Housing Available", 
+                subject="Purdue Housing Bot: New Housing Available",
                 body=(
                     "There is new housing available. Login here: "
                     "https://purdue.starrezhousing.com/StarRezPortalX/"
                 )
             )
 
-    def screen_to_string(self) -> str | None:  
+    def screen_to_string(self) -> str | None:
+        """Function to convert the screen to a string"""
         try:
             pytesseract.pytesseract.tesseract_cmd = self.tesseract_location.get()
             cap = ImageGrab.grab()
             result = pytesseract.image_to_string(
-                cv2.cvtColor(np.array(cap), cv2.COLOR_BGR2GRAY), 
+                cv2.cvtColor(np.array(cap), cv2.COLOR_BGR2GRAY),
                 lang='eng'
-                ) 
+            )
             return result
-        except:
+        except BaseException:
             messagebox.showerror(
-                title="Error", 
+                title="Error",
                 message=(
                     "Failed to convert screen to text. "
                     "Check tesseract location."
                 )
-                )
+            )
             self.stop_program()
             return
 
     def send_test_email(self):
+        """Function to send a test email"""
         if self.validate_inputs():
             self.send_email(
-                subject="Purdue Housing Bot: Test Email", 
+                subject="Purdue Housing Bot: Test Email",
                 body=(
                     "This is a test email. "
                     "Please check that all emails recieved the message."
-                    )
                 )
+            )
             self.message.set("Test email sent.")
             self.message_label.config(foreground="#66ff00")
             self.message_label.config(font="TkDefaultFont 14")
             self.window.after(5000, self.reset_message)
 
     def reset_message(self):
+        """Function to reset the message label"""
         self.message.set("Not Running.")
         self.message_label.config(foreground="black")
         self.message_label.config(font="TkDefaultFont 10")
 
     def send_email(self, subject: str, body: str):
+        """Function to send an email"""
         msg = EmailMessage()
         msg['Subject'] = subject
         msg['From'] = self.sender_email.get()
@@ -303,27 +325,28 @@ class PurdueHousingBot:
         msg.set_content(body)
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(
-            host='smtp.gmail.com', 
-            port=465, 
+            host='smtp.gmail.com',
+            port=465,
             context=context
-            ) as server:
+        ) as server:
             try:
                 server.login(
-                    self.sender_email.get(), 
+                    self.sender_email.get(),
                     self.sender_password.get()
-                    )
+                )
                 server.send_message(msg)
-            except:
+            except BaseException:
                 messagebox.showerror(
-                    title="Error", 
+                    title="Error",
                     message=(
                         "Failed to send email. "
                         "Check emails and password."
-                        )
-                    ) 
+                    )
+                )
                 self.stop_program()
 
+
 if __name__ == "__main__":
-    root = tk.Tk()  
-    app = PurdueHousingBot(root)
+    root = tk.Tk()
+    app = PurdueHousingBot(master=root)
     root.mainloop()
